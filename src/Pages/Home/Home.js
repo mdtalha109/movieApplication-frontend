0fbb902e470fa5ea2fb11d7889e22b35c8c625b4
Md Talha <mdtalha1041@gmail.com>
2022-06-01 22:33:16 +0530

    added public list

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { userContext } from '../../App';
-import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+import { DeleteIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Loader from '../../Components/Loader/Loader';
@@ -13,19 +13,24 @@ const Home = () => {
 
   const [movieKeyword, setMovieKeyword] = useState('')
   const [createdMovieList, setCreatedMovieList] = useState({createdList: []})
-  const [loading, setLoading] = useState(false)
+  const [createdMovieListPublic, setCreatedMovieListPublic] = useState({createdListPublic: []})
+  const [loadingPrivate, setLoadingPrivate] = useState(false)
+  const [loadingPublic, setLoadingPublic] = useState(false)
 
 
     const navigate = useNavigate()
     const {state, dispatch} = useContext(userContext);
     useEffect(()=> {
       
+      
         if(!state){
             navigate('/')
         }
+        console.log(state)
 
-        const fetchMovieList = async() => {
-          setLoading(true);
+
+        const fetchMovieListPublic = async() => {
+          setLoadingPublic(true);
           console.log('fetching created list')
           const config = {
             headers: {
@@ -34,12 +39,35 @@ const Home = () => {
             }
           }
     
-          const {data} = await axios.get('https://movie-app-pro.herokuapp.com/api/list/getlist',config);
-          setCreatedMovieList({createdList: data})
-          setLoading(false);
+          const {data} = await axios.get('https://movie-app-pro.herokuapp.com/api/list/getlist/public',config);
+          console.log(data)
+          
+          setCreatedMovieListPublic({createdListPublic: data})
+          setLoadingPublic(false);
         }
     
-        fetchMovieList();
+
+        const fetchMovieListPrivate = async() => {
+          setLoadingPrivate(true);
+          console.log('fetching created list')
+          const config = {
+            headers: {
+                'Content-Type' : ' application/json',
+                'Authorization' : `Bearer ${state.token}`
+            }
+          }
+    
+          const {data} = await axios.get('https://movie-app-pro.herokuapp.com/api/list/getlist/private',config);
+          console.log(data)
+          
+          setCreatedMovieList({createdList: data})
+          fetchMovieListPublic();
+          setLoadingPrivate(false);
+        }
+    
+        fetchMovieListPrivate();
+
+
 
 
     }, [])
@@ -57,9 +85,14 @@ const Home = () => {
             'Authorization' : `Bearer ${state.token}`
         }
       }
+
+      toast('Wait, Creating List', {
+        autoClose: 1000,
+      });
       const {data} = await axios.post('https://movie-app-pro.herokuapp.com/api/list/createList', {movieListName:newListName}, config)
       console.log(data.createdList.movieListName)
       setCreatedMovieList({createdList: [...createdMovieList.createdList, data.createdList]})
+      setCreatedMovieListPublic({createdListPublic: [...createdMovieListPublic.createdListPublic, data.createdList]})
       toast.success('List created successfully', {
         autoClose: 2000,
       });
@@ -76,8 +109,44 @@ const Home = () => {
       // console.log(data.Search)
       // setMovieList({movieListItems: data.Search})
       navigate(`/search?q=${movieKeyword}`)
+    }
 
+    const deleteListHandler = async(e, movieListName) => {
+        
+        e.preventDefault()
+        const config = {
+          headers: {
+              'Content-Type' : ' application/json',
+              'Authorization' : `Bearer ${state.token}`
+          }
+        }
 
+        const updatedList = createdMovieList.createdList.filter((item)=>{
+          if(item.movieListName == movieListName){
+            return;
+          }
+          else return item;
+        } )
+        setCreatedMovieList({createdList: updatedList})
+
+        const updatedListPublic = createdMovieListPublic.createdListPublic.filter((item)=>{
+          if(item.movieListName == movieListName){
+            return;
+          }
+          else return item;
+        } )
+        setCreatedMovieListPublic({createdListPublic: updatedListPublic})
+        
+        const {data} = await axios.post('https://movie-app-pro.herokuapp.com/api/list/deletelist',{movieListName},config);
+        if(data.acknowledged){
+          
+          toast.success('List deleted Successfully')
+        }
+        else{
+          toast.warning('Something went wrong, try again!')
+        }
+        
+        
     }
   return (
     <>
@@ -91,19 +160,40 @@ const Home = () => {
             </div>
 
             <div className='created-movie-list'>
-            <h2>Created Movie List:</h2>  <button onClick={newListHandler}>Create new List</button>
+            <h2>Created Movie List (Private):</h2>  <button onClick={newListHandler}>Create new List</button>
             <div className='movie-list-container'>
                 {
-                  loading ? <div style={{marginTop:"20px"}}><p>Loading your List</p><Loader/></div>:
+                  loadingPrivate ? <div style={{marginTop:"20px"}}><p>Loading your List</p><Loader/></div>:
                   createdMovieList && createdMovieList.createdList.map((item) => 
                   <Link to={`/list?q=${item.movieListName}`}>
                     <div className='movie-list-item'>
-                      <h3>{item.movieListName}<PhoneIcon /></h3>
+                      <h3>{item.movieListName} <ArrowForwardIcon/></h3>
+                      {
+                        item.createdBy == state.id ? <div onClick={(e)=>deleteListHandler(e, item.movieListName)} className='delete-container'><DeleteIcon/></div> : ''
+                      }                  
                     </div>
-                  </Link>
-                   
+                  </Link>        
                   )
                 }
+                {
+                  !loadingPrivate && createdMovieList.createdList.length == 0 && <p>your private list is empty</p>
+                }
+              
+            </div>
+
+            <h2>Created Movie List (Public):</h2>
+            <div className='movie-list-container'>
+                {
+                  loadingPublic ? <div style={{marginTop:"20px"}}><p>Loading Public List</p><Loader/></div>:
+                  createdMovieListPublic && createdMovieListPublic.createdListPublic.map((item) => 
+                  <Link to={`/list?q=${item.movieListName}`}>
+                    <div className='movie-list-item'>
+                      <h3>{item.movieListName} <ArrowForwardIcon/></h3>                  
+                    </div>
+                  </Link>        
+                  )
+                }
+                
               
             </div>
             </div> 
